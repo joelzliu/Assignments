07ab45baf2867199dba30d389e815daab92c2871
joelzliu <joelzliu@gmail.com>
2024-12-06 01:13:03 -0600

    Finished Task 1 & 2

---
+++ C#_Assignment/ConsoleApp1/02UnderstandingTypes/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("{0,-10} {1,-8} {2,-30} {3,-30}", "Type", "Bytes", "Min Value", "Max Value");
+Console.WriteLine(new string('-', 80)); // Separator line
+
+Console.WriteLine("{0,-10} {1,-8} {2,-30} {3,-30}", "sbyte", sizeof(sbyte), sbyte.MinValue, sbyte.MaxValue);
+Console.WriteLine("{0,-10} {1,-8} {2,-30} {3,-30}", "byte", sizeof(byte), byte.MinValue, byte.MaxValue);
+Console.WriteLine("{0,-10} {1,-8} {2,-30} {3,-30}", "short", sizeof(short), short.MinValue, short.MaxValue);
+Console.WriteLine("{0,-10} {1,-8} {2,-30} {3,-30}", "ushort", sizeof(ushort), ushort.MinValue, ushort.MaxValue);
+Console.WriteLine("{0,-10} {1,-8} {2,-30} {3,-30}", "int", sizeof(int), int.MinValue, int.MaxValue);
+Console.WriteLine("{0,-10} {1,-8} {2,-30} {3,-30}", "uint", sizeof(uint), uint.MinValue, uint.MaxValue);
+Console.WriteLine("{0,-10} {1,-8} {2,-30} {3,-30}", "long", sizeof(long), long.MinValue, long.MaxValue);
+Console.WriteLine("{0,-10} {1,-8} {2,-30} {3,-30}", "ulong", sizeof(ulong), ulong.MinValue, ulong.MaxValue);
+Console.WriteLine("{0,-10} {1,-8} {2,-30} {3,-30}", "float", sizeof(float), float.MinValue, float.MaxValue);
+Console.WriteLine("{0,-10} {1,-8} {2,-30} {3,-30}", "double", sizeof(double), double.MinValue, double.MaxValue);
+Console.WriteLine("{0,-10} {1,-8} {2,-30} {3,-30}", "decimal", sizeof(decimal), decimal.MinValue, decimal.MaxValue);
---
+++ C#_Assignment/ConsoleApp1/convertCenturies/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Enter the number of centuries: ");
+int centuries = int.Parse(Console.ReadLine());
+
+int years = centuries * 100;
+int days = years * 3652425 / 10000; // 365.2425
+long hours = days * 24L;
+long minutes = hours * 60L;
+long seconds = minutes * 60L;
+long milliseconds = seconds * 1000L;
+ulong microseconds = (ulong)milliseconds * 1000;
+ulong nanoseconds = microseconds * 1000;
+
+Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes " +
+                   $"= {seconds} seconds = {milliseconds} milliseconds = {microseconds} microseconds = {nanoseconds} nanoseconds");
